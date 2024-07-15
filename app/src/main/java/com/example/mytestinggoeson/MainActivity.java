c7acf5f803445f378a567b9228a926e9d7826d89
MikaDevelops <mika.virtala@gmail.com>
2024-07-15 11:54:59 +0300

    data to text file

--- app/src/main/java/com/example/mytestinggoeson/MainActivity.java
+++ app/src/main/java/com/example/mytestinggoeson/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.mytestinggoeson;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,24 +29,49 @@ import androidx.core.view.WindowInsetsCompat;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText serialTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         serialTxt = findViewById(R.id.serialTxt);
+        Button btnSave = findViewById(R.id.btnSave);
         Button btnScan = findViewById(R.id.btnScan);
+
         btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ScanOptions scanOptions = new ScanOptions();
                 barcodeLauncher.launch(scanOptions);
 
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    writeRowToFile(serialTxt.getText().toString());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -54,5 +92,25 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
     );
+
+    protected void writeRowToFile(String row) throws IOException {
+        File[] path = getApplicationContext().getExternalMediaDirs();
+        File thePathToFile = new File(path[0], "data.txt");
+
+//        try (FileWriter writer = new FileWriter(thePathToFile, true)) {
+//            writer.append(row);
+//            writer.close();
+//            Toast.makeText(getApplicationContext(), "data written into data.txt", Toast.LENGTH_SHORT).show();
+//        }
+
+//        try (FileOutputStream writer = new FileOutputStream(thePathToFile, true)) {
+//            writer.write(row.getBytes(StandardCharsets.UTF_8));
+//            Toast.makeText(getApplicationContext(), "data written into data.txt", Toast.LENGTH_SHORT).show();
+//        }
+        try (FileOutputStream fout = openFileOutput(thePathToFile.getPath(), MODE_APPEND)){
+            fout.write(row.getBytes());
+        }
+
+    }
 
 }
